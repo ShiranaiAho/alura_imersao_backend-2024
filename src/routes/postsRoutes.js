43dd31b3342da10560d360b48e72d1ba42c8aaf0
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2024-11-22 15:32:23 -0300

    Day 5 - Integration Front/Back

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
 import multer from "multer";
+import cors from "cors";
 import { listPosts, postNewPost, uploadImg, updateNewPost } from "../controllers/postsController.js";
+
+const corsOpt = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+};
 
 const upload = multer({dest: "./uploads"});
 
 const routes = (app) => {
     app.use(express.json());
+    app.use(cors(corsOpt));
     app.get("/posts", listPosts);
     app.post("/posts", postNewPost);
     app.post("/upload", upload.single("image"), uploadImg);
